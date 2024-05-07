17021dceaf9ce5bf2019e5ab02176d26c34844ef
DanielDaze <tattoo255@yandex.ru>
2024-05-07 21:58:59 +0300

    - добавил логирование в ErrorHandler

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
@@ -17,18 +17,21 @@ public class ErrorHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public Map<String, String> handleNotValidException(MethodArgumentNotValidException e) {
         String message = "Команда не была выполнена, так как данные были введены неправильно";
+        log.error("Во время выполнении команды пользователя произошла ошибка, т.к. данные были введены неправильно");
         return Map.of("Ошибка", message);
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public Map<String, String> handleNotFoundException(NotFoundException e) {
+        log.error("Во время выполнении команды пользователя произошла ошибка, т.к. пользователь пытался найти несуществующие данные");
         return Map.of("Ошибка", e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public Map<String, String> handleThrowable(Throwable e) {
+        log.error("Во время выполнении команды пользователя произошла ошибка на стороне сервера");
         return Map.of("Ошибка", "Что-то пошло не так");
     }
 }
